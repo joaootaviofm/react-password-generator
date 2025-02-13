517ea53929a298be24963e412f7e40870571c4a3
joaootaviofm <joaootaviofm@gmail.com>
2025-02-13 18:31:22 -0300

    bugfix

--- src/Password.jsx
+++ src/Password.jsx
@@ -2,10 +2,10 @@ export default function Password(){
     return(
         <div className="">
             <h1 className="text-center font-bold text-white text-[2rem] mb-5">Password Generator</h1>
-            <div className="flex flex-col gap-3 text-white bg-teal-900 rounded p-10">
+            <div className="flex flex-col gap-3 text-white bg-teal-900 rounded p-10 shadow-md shadow-black">
                 <div className="flex gap-10 items-center hover:opacity-50 transition-opacity duration-500 ">
                     <label className="flex-1" htmlFor="passwordLength">Password length</label>
-                    <input type="number" id="passwordLength" className="bg-white rounded text-black"/>
+                    <input type="number" id="passwordLength" className="bg-white rounded text-black outline-0 border-1 text-center border-black"/>
                 </div>
                 <div className="flex items-center hover:opacity-50 transition-opacity duration-500" >
                     <label className="flex-1" htmlFor="includeUppercase">Include uppercase letters</label>
@@ -21,7 +21,7 @@ export default function Password(){
                 </div>  
                 <div className="flex items-center hover:opacity-50 transition-opacity duration-500">
                     <label className="flex-1" htmlFor="includeSymbols">Include symbols</label>
-                    <input type="checkbox" id="includeSymbols" />
+                    <input type="checkbox" id="includeSymbols"/>
                 </div>  
             </div>
         </div>
