4a7d85939c7354bbe2b22a664be4dd79383874b2
joaootaviofm <joaootaviofm@gmail.com>
2025-02-13 18:31:15 -0300

    added font montserrat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css'
 import Password from './Password'
 function App() {
 
   return (
-    <div className='bg-slate-900 w-full min-h-screen flex flex-col items-center justify-center'>
+    <div className='font-display bg-slate-900 w-full min-h-screen flex flex-col items-center justify-center m-0 p-0 box-border'>
       <Password />
     </div>
   )
