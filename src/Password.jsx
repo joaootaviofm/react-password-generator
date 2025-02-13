d4170d857dfb42df4a07db72abb4be3a4dec99d8
joaootaviofm <joaootaviofm@gmail.com>
2025-02-13 19:46:50 -0300

    added input to copy the generated password

--- src/Password.jsx
+++ src/Password.jsx
@@ -46,13 +46,11 @@ export default function Password(){
         }
      }
 
-
-
     return(
-        <div className="">
+        <div>
             <h1 className="text-center font-bold text-white text-[2rem] mb-5">Password Generator</h1>
             <div className="flex flex-col gap-3 text-white bg-teal-900 rounded p-10 shadow-md shadow-black">
-                <input type="text" />
+                <input className='bg-white text-black rounded outline-0 text-center' placeholder='Your password' type="text" readOnly/>
                 <div className="flex gap-10 items-center hover:opacity-50 transition-opacity duration-500 ">
                     <label className="flex-1" htmlFor="passwordLength">Password length</label>
                     <input onChange={handlePasswordLength} placeholder="Password length" type="number" id="passwordLength" className="bg-white rounded text-black outline-0 border-1 text-center border-black"/>
