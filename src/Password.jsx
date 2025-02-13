5c325726f30b5efe40a451f63a954ee2a875b2b4
joaootaviofm <joaootaviofm@gmail.com>
2025-02-13 19:23:01 -0300

    added functions and useState variables

--- src/Password.jsx
+++ src/Password.jsx
@@ -1,28 +1,79 @@
+import { useState } from 'react'
+
 export default function Password(){
+    
+    const [passwordLength, setPasswordLength] = useState()
+    const [includeUpperCase, setIncludeUpperCase] = useState(false)
+    const [includeLowerCase, setIncludeLowerCase] = useState(false)
+    const [includeNumbers, setIncludeNumbers] = useState(false)
+    const [includeSymbols, setIncludeSymbols] = useState(false)
+
+    function generatePassword(){
+        alert(passwordLength)
+    }
+
+     function handlePasswordLength(event){
+        setPasswordLength(event.target.value)
+     }
+
+     function handleIncludeUpperCase(event){
+        if(event.target.checked){
+            setIncludeUpperCase(true)
+        }
+     }
+
+     function handleIncludeLowerCase(event){
+        if(event.target.checked){
+            setIncludeLowerCase(true)
+        }
+     }
+     
+     function handleIncludeLowerCase(event){
+        if(event.target.checked){
+            setIncludeLowerCase(true)
+        }
+     }
+
+     function handleIncludeNumbers(event){
+        if(event.target.checked){
+            setIncludeNumbers(true)
+        }
+     }
+     
+     function handleIncludeSymbols(event){
+        if(event.target.checked){
+            setIncludeSymbols(true)
+        }
+     }
+
+
+
     return(
         <div className="">
             <h1 className="text-center font-bold text-white text-[2rem] mb-5">Password Generator</h1>
             <div className="flex flex-col gap-3 text-white bg-teal-900 rounded p-10 shadow-md shadow-black">
+                <input type="text" />
                 <div className="flex gap-10 items-center hover:opacity-50 transition-opacity duration-500 ">
                     <label className="flex-1" htmlFor="passwordLength">Password length</label>
-                    <input type="number" id="passwordLength" className="bg-white rounded text-black outline-0 border-1 text-center border-black"/>
+                    <input onChange={handlePasswordLength} placeholder="Password length" type="number" id="passwordLength" className="bg-white rounded text-black outline-0 border-1 text-center border-black"/>
                 </div>
                 <div className="flex items-center hover:opacity-50 transition-opacity duration-500" >
                     <label className="flex-1" htmlFor="includeUppercase">Include uppercase letters</label>
-                    <input type="checkbox" id="includeUppercase" />
+                    <input onChange={handleIncludeUpperCase} type="checkbox" id="includeUppercase" />
                 </div>      
                 <div className="flex items-center hover:opacity-50 transition-opacity duration-500">
                     <label className="flex-1" htmlFor="includeLowercase">Include lowercase letters</label>
-                    <input type="checkbox" id="includeLowercase" />
+                    <input onChange={handleIncludeLowerCase} type="checkbox" id="includeLowercase" />
                 </div>            
                 <div className="flex items-center hover:opacity-50 transition-opacity duration-500">
                     <label className="flex-1" htmlFor="includeNumber">Include numbers</label>
-                    <input type="checkbox" id="includeNumber" />
+                    <input onChange={handleIncludeNumbers} type="checkbox" id="includeNumber" />
                 </div>  
                 <div className="flex items-center hover:opacity-50 transition-opacity duration-500">
                     <label className="flex-1" htmlFor="includeSymbols">Include symbols</label>
-                    <input type="checkbox" id="includeSymbols"/>
+                    <input onChange={handleIncludeSymbols}type="checkbox" id="includeSymbols"/>
                 </div>  
+                <button onClick={generatePassword} className="bg-slate-900 rounded p-1 cursor-pointer hover:opacity-85 transition-opacity duration-500">Generate password</button>
             </div>
         </div>
     )
