557305ad2d068d542029d2d8f369f8120b0d0ff9
joaootaviofm <joaootaviofm@gmail.com>
2025-02-14 19:03:38 -0300

    password error

---
+++ src/PasswordError.jsx
@@ -0,0 +1,15 @@
+import { useState } from  'react'
+export default function PasswordError(){
+
+    const [showDiv, setShowDiv] = useState(true)
+
+    if(!showDiv) return null;
+    return(
+        <>
+            <div className="text-green-400 border-teal-900 border-dashed bg-black border-2 text-center w-96 p-10 fixed top-10 left-1/2 -translate-x-1/2">
+                <p>Check your password length!</p>
+                <button onClick={() => setShowDiv(false)} className="cursor-pointer bg-green-400 text-white font-bold w-[120px] rounded mt-5 hover:opacity-80 transition-opacity duration-75">Ok</button>
+            </div>
+        </>
+    )
+}
